f18ff64cc96e6b8968afc339ded27cfd80d8c929
Gege <gegehuang.gh@gmail.com>
2016-11-16 19:26:08 -0500

    Create ex34.py

---
+++ Learn-Python-The-Hard-Way/ex34.py
@@ -0,0 +1,17 @@
+animals = ['bear', 'python', 'peacock', kangraoo', 'whale', 'platypus']
+i = 0
+while i < 6:
+  if i == 0:
+    print "The 1st animal is at %d and is a %s." % i, animals[i]
+    i++
+  elif i == 1:
+    print "The 2nd animal is at %d and is a %s." % i, animals[i]
+    i++
+  elif i == 2:
+    print "The 3rd animal is at %d and is a %s." % i, animals[i]
+    i++
+  else:
+    print "The %dst animal is at %d and is a %s." % (i + 1), i, animals[i]
+    i++
+    
+   
